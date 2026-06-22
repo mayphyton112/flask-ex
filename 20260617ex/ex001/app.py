09cc10637cf81b763f8431b8c334ce662304c8aa
mayphyton112 <mayphyton112@gmail.com>
2026-06-22 17:28:38 +0900

    20260622 DANGERZONE by ktj 17:28

--- 20260617ex/ex001/app.py
+++ 20260617ex/ex001/app.py
@@ -35,6 +35,7 @@ def get_user(user_id):
 @app.route('/users/<string:name>')
 def get_name(name):
     return f'{name}님 요청'
+
 # http://127.0.0.1:5000/pi/3.14
 @app.route('/pi/<float:pi>')
 def get_pi(pi):
